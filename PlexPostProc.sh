437a2f40613704969b798f7f6f92300dcc17c4e1
cbron <calebbron@gmail.com>
2021-03-06 21:10:02 -0700

    modified for remuxing

--- PlexPostProc.sh
+++ PlexPostProc.sh
@@ -36,7 +36,7 @@
 #
 #******************************************************************************
 
-TMPFOLDER="/tmp"
+TMPFOLDER="/mnt/Media/transcoding/tmp/"
 ENCODER="ffmpeg"  # Encoder to use:
                   # "ffmpeg" for FFMPEG [DEFAULT]
                   # "handbrake" for HandBrake
@@ -87,43 +87,13 @@ if [ ! -z "$1" ]; then
    # ********************************************************
 
    echo "$(date +"%Y%m%d-%H%M%S"): Starting transcode of $FILENAME to $TEMPFILENAME" | tee -a $LOGFILE
-   if [[ $ENCODER == "handbrake" ]]; then
-     echo "You have selected HandBrake" | tee -a $LOGFILE
-     HandBrakeCLI -i "$FILENAME" -f mkv --aencoder copy -e qsv_h264 --x264-preset veryfast --x264-profile auto -q 16 --maxHeight $RES --decomb bob -o "$TEMPFILENAME"
-     check_errs $? "Failed to convert using Handbrake."
-   elif [[ $ENCODER == "ffmpeg" ]]; then
-     echo "You have selected FFMPEG" | tee -a $LOGFILE
-     ffmpeg -i "$FILENAME" -s hd$RES -c:v libx264 -preset veryfast -vf yadif -c:a copy "$TEMPFILENAME"
-     check_errs $? "Failed to convert using FFMPEG."
-	 elif [[ $ENCODER == "nvtrans" ]]; then
-     export FFMPEG_EXTERNAL_LIBS="$(find ~/Library/Application\ Support/Plex\ Media\ Server/Codecs/ -name "libmpeg2video_decoder.so" -printf "%h\n")/"
-     check_errs $? "Failed to convert using smart Plex Transcoder (NVENC). libmpeg2video_decoder.so not found."
-     export LD_LIBRARY_PATH="/usr/lib/plexmediaserver:/usr/lib/plexmediaserver/lib/"
+   echo "You have selected FFMPEG" | tee -a $LOGFILE
+   # ffmpeg -i "$FILENAME" -s hd$RES -c:v libx264 -preset veryfast -vf yadif -c:a copy "$TEMPFILENAME"
 
-     # Grab some dimension and framerate info so we can set bitrates
-     HEIGHT="$(/usr/lib/plexmediaserver/Plex\ Transcoder -i "$FILENAME" 2>&1 | grep "Stream #0:0" | perl -lane 'print $1 if /, \d{3,}x(\d{3,})/')"
-     FPS="$(/usr/lib/plexmediaserver/Plex\ Transcoder -i "$FILENAME" 2>&1 | grep "Stream #0:0" | perl -lane 'print $1 if /, (\d+(.\d+)*) fps/')"
+   # this only re-muxes
+   ffmpeg -i "$FILENAME" -c copy "$TEMPFILENAME"
 
-     if [[ -z "${HEIGHT}" ]]; then
-	     # Failed to get dimensions of source... try a dumb transcode.
-	     /usr/lib/plexmediaserver/Plex\ Transcoder -y -hide_banner -hwaccel nvdec -i "$FILENAME" -s hd$RES -c:v h264_nvenc -preset veryfast -c:a copy "$TEMPFILENAME"
-	     check_errs $? "Failed to convert using simple Plex Transcoder (NVENC)."
-	   else
-	     # Smart transcode based on source dimensions and fps
-       # Bitrate vlaues (Mb). Assuming 1080i30 (ATSC max) has same needs as 720p60
-       # Assuming 60 fps needs 2x bitrate than 30 fps
-       MULTIPLIER=$(echo - | perl -lane "if (${FPS} < 59) {print 1.0} else {print 2.0}")
-       ABR=$(echo - | perl -lane "if (${HEIGHT} < 720) {print 1*${MULTIPLIER}} elsif (${HEIGHT} < 1080) {print 2*${MULTIPLIER}} else {print 4*${MULTIPLIER}}")
-       MBR=$(echo - | perl -lane "print ${ABR} * 1.5")
-       BUF=$(echo - | perl -lane "print ${MBR} * 2.0")
-       /usr/lib/plexmediaserver/Plex\ Transcoder -y -hide_banner -hwaccel nvdec -i "$FILENAME" -c:v h264_nvenc -b:v "${ABR}M" -maxrate:v "${MBR}M" -profile:v high -bf:v 3 -bufsize:v "${BUF}M" -preset:v hq -forced-idr:v 1 -c:a copy "$TEMPFILENAME"
-	     check_errs $? "Failed to convert using smart Plex Transcoder (NVENC)."
-	  fi
-   else
-     echo "Oops, invalid ENCODER string.  Using Default [FFMpeg]." | tee -a $LOGFILE
-     ffmpeg -i "$FILENAME" -s hd$RES -c:v libx264 -preset veryfast -vf yadif -c:a copy "$TEMPFILENAME"
-     check_errs $? "Failed to convert using FFMPEG."
-   fi
+   check_errs $? "Failed to convert using FFMPEG."
 
    # ********************************************************"
    # Encode Done. Performing Cleanup
@@ -131,8 +101,8 @@ if [ ! -z "$1" ]; then
 
    echo "$(date +"%Y%m%d-%H%M%S"): Finished transcode of $FILENAME to $TEMPFILENAME" | tee -a $LOGFILE
 
-   rm -f "$FILENAME" # Delete original in .grab folder
-   check_errs $? "Failed to remove original file: $FILENAME"
+   # rm -f "$FILENAME" # Delete original in .grab folder
+   # check_errs $? "Failed to remove original file: $FILENAME"
 
    mv -f "$TEMPFILENAME" "${FILENAME%.ts}.mkv" # Move completed tempfile to .grab folder/filename
    check_errs $? "Failed to move converted file: $TEMPFILENAME"
